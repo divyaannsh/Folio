d1b8edf2dda7e2e8de9e4dc28eaf6a6a2a513d11
Divyansh <divyanshsrivastav72@gmail.com>
2024-04-08 16:55:32 +0530

    Blogs Updated

--- Pages/blogs-projects.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.toggle-button');
-const navBarLinks = document.querySelector('.nav-links');
-
-toggleBtn.addEventListener('click', () => {
-  navBarLinks.classList.toggle('nav-links-active');
-})
